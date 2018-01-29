c977ac2d66d2da06f31b08d1c3927cb7ad253368
nerdy-adams <nerdrun.adams@gmail.com>
2018-01-29 13:52:18 +0900

    #2 커스터마이징 - header , section , footer

---
+++ README.txt
@@ -0,0 +1,17 @@
+
+CREDITS:
+
+Bootstrap
+http://getbootstrap.com/
+
+jQuery
+http://jquery.com/
+
+Google Fonts
+https://www.google.com/fonts/
+
+
+WOW.js
+
+Demo Images:
+http://unsplash.com
---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+let jsonParser = bodyParser.json()
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use('/public', express.static(__dirname + '/public'));
+app.set('views', __dirname+'/views')
+app.set('view engine', 'pug');
+app.get(['/','/about','/contact'], (req,res) => {
+  let curentURL = req.path;
+  console.log(curentURL);
+  res.render('index',{url:curentURL})
+})
+app.listen(3000, () => {
+  console.log('start serve 3000 port');
+})
